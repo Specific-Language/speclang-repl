ed59d2a2d6f06a0a7258b97c0b1f6abb8296328c
sallaben <steven.allaben@gmail.com>
2022-05-18 20:01:58 -0400

    criswell commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { parse } from 'speclang';
+
+	let input = `test {
+    hello world {}
+    foo = "bar"
+  }`;
+	let output: string;
+
+	async function test() {
+		output = await parse(input);
+	}
+</script>
+
+<main>
+	<h1>Specific Language REPL</h1>
+	<div>
+		<textarea bind:value={input} rows="12" />
+		<button on:click={test}>parse</button>
+	</div>
+	<hr />
+	<pre>{output}</pre>
+</main>
